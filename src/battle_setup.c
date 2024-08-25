1c8dc236ac43d5167283833b96d7b86090f30cc7
CyanSMP64 <jmleuschke64@yahoo.com>
2024-08-26 09:53:40 +1200

    fix crash

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -214,40 +214,6 @@ static void CreateBattleStartTask(u8 transition, u16 song) // song == 0 means de
     PlayMapChosenOrBattleBGM(song);
 }
 
-static void Task_BattleStart_Debug(u8 taskId)
-{
-    s16 *data = gTasks[taskId].data;
-
-    switch (tState)
-    {
-    case 0:
-        if (!FldEffPoison_IsActive()) // is poison not active?
-        {
-            BattleTransition_StartOnField(tTransition);
-            tState++; // go to case 1.
-        }
-        break;
-    case 1:
-        if (IsBattleTransitionDone() == TRUE)
-        {
-            CleanupOverworldWindowsAndTilemaps();
-            SetMainCallback2(CB2_InitBattle);
-            RestartWildEncounterImmunitySteps();
-            ClearPoisonStepCounter();
-            DestroyTask(taskId);
-        }
-        break;
-    }
-}
-
-static void CreateBattleStartTask_Debug(u8 transition, u16 song)
-{
-    u8 taskId = CreateTask(Task_BattleStart_Debug, 1);
-
-    gTasks[taskId].tTransition = transition;
-    PlayMapChosenOrBattleBGM(song);
-}
-
 static bool8 CheckSilphScopeInPokemonTower(u16 mapGroup, u16 mapNum)
 {
     if (mapGroup == MAP_GROUP(POKEMON_TOWER_1F)
@@ -284,16 +250,6 @@ static void DoStandardWildBattle(void)
     CreateBattleStartTask(GetWildBattleTransition(), 0);
     IncrementGameStat(GAME_STAT_TOTAL_BATTLES);
     IncrementGameStat(GAME_STAT_WILD_BATTLES);
-}
-
-void DoStandardWildBattle_Debug(void)
-{
-    LockPlayerFieldControls();
-    FreezeObjectEvents();
-    StopPlayerAvatar();
-    gMain.savedCallback = CB2_EndWildBattle;
-    gBattleTypeFlags = 0;
-    CreateBattleStartTask_Debug(GetWildBattleTransition(), 0);
 }
 
 void StartRoamerBattle(void)
@@ -415,6 +371,50 @@ void StartLegendaryBattle(void)
     }
     IncrementGameStat(GAME_STAT_TOTAL_BATTLES);
     IncrementGameStat(GAME_STAT_WILD_BATTLES);
+}
+
+static void Task_BattleStart_Debug(u8 taskId)
+{
+    s16 *data = gTasks[taskId].data;
+
+    switch (tState)
+    {
+    case 0:
+        if (!FldEffPoison_IsActive()) // is poison not active?
+        {
+            BattleTransition_StartOnField(tTransition);
+            tState++; // go to case 1.
+        }
+        break;
+    case 1:
+        if (IsBattleTransitionDone() == TRUE)
+        {
+            CleanupOverworldWindowsAndTilemaps();
+            SetMainCallback2(CB2_InitBattle);
+            RestartWildEncounterImmunitySteps();
+            ClearPoisonStepCounter();
+            DestroyTask(taskId);
+        }
+        break;
+    }
+}
+
+static void CreateBattleStartTask_Debug(u8 transition, u16 song)
+{
+    u8 taskId = CreateTask(Task_BattleStart_Debug, 1);
+
+    gTasks[taskId].tTransition = transition;
+    PlayMapChosenOrBattleBGM(song);
+}
+
+void DoStandardWildBattle_Debug(void)
+{
+    LockPlayerFieldControls();
+    FreezeObjectEvents();
+    StopPlayerAvatar();
+    gMain.savedCallback = CB2_EndWildBattle;
+    gBattleTypeFlags = 0;
+    CreateBattleStartTask_Debug(GetWildBattleTransition(), 0);
 }
 
 void StartGroudonKyogreBattle(void)
