0d41693c3e538777129554f3339b076e51d19d85
CyanSMP64 <jmleuschke64@yahoo.com>
2026-02-21 17:14:21 +1300

    filler 206

--- include/constants/global.h
+++ include/constants/global.h
@@ -34,7 +34,7 @@
 #define NATDEX_VERSION_MINOR 1
 #define NATDEX_VERSION_PATCH 3
 // revision number: git describe --tags
-#define NATDEX_VERSION_BUILD 205
+#define NATDEX_VERSION_BUILD 206
 
 // capacities of various saveblock objects
 #define DAYCARE_MON_COUNT   2
