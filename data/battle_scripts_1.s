3df87085cb9f12b2f28c1dad8ed7b1407fe34baa
CyanSMP64 <jmleuschke64@yahoo.com>
2026-03-17 23:46:34 +1300

    remove smoke ball animation

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -3046,7 +3046,6 @@ BattleScript_BattleTowerEtcTrainerBattleWonSkipText::
 	end2
 
 BattleScript_SmokeBallEscape::
-	playanimation BS_ATTACKER, B_ANIM_SMOKEBALL_ESCAPE
 	printstring STRINGID_PKMNFLEDUSINGITS
 	waitmessage 0
 	end2
